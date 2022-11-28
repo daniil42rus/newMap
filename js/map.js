46b4f85bab22566d3c4cf9d1a6a461aeb6af02b6
Даниил Кулешов <daniil42rus@mail.ru>
2022-11-29 01:51:41 +0700

    second commit

--- js/map.js
+++ js/map.js
@@ -6,4 +6,99 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	// attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+///marker ///
 
+let polygon = [];
+let howManyMarkersInput = document.getElementById('howManyMarkers');
+
+function addMarker(e) {
+	console.log(polygon);
+	lat = e.latlng.lat;
+	lng = e.latlng.lng;
+	console.log(lat);
+	console.log(lng);
+	L.marker([lat, lng]).addTo(map);
+	map.off('click', addMarker);
+
+	let xp = [];
+	let yp = [];
+
+	polygon.forEach((element) => {
+		xp.push(element[0]);
+		yp.push(element[1]);
+	});
+
+	console.log(xp);
+	console.log(yp);
+
+	let x = lat;
+	let y = lng;
+
+	function inPoly(x, y) {
+		let npol = xp.length;
+		let j = npol - 1;
+		let c = 0;
+		for (let i = 0; i < npol; i++) {
+			if (
+				((yp[i] <= y && y < yp[j]) || (yp[j] <= y && y < yp[i])) &&
+				x > ((xp[j] - xp[i]) * (y - yp[i])) / (yp[j] - yp[i]) + xp[i]
+			) {
+				c = !c;
+			}
+			j = i;
+		}
+		return c;
+	}
+	if (inPoly(x, y)) {
+		howManyMarkersInput.value++;
+	}
+
+	console.log(inPoly(x, y));
+}
+
+let addMarkerBtn = document.getElementById('addMarker');
+addMarkerBtn.addEventListener('click', async () => {
+	console.log('addMarkerBtn');
+	await map.on('click', addMarker);
+});
+
+/// polygin ///
+
+let latlngs = [];
+
+function addPolygon(e) {
+	let dot = L.icon({
+		iconUrl: './style/images/dot.png',
+		iconSize: [10, 10],
+	});
+	let lat = e.latlng.lat;
+	let lng = e.latlng.lng;
+	console.log(lat);
+	console.log(lng);
+
+	latlngs.push([lat, lng]);
+	console.log(latlngs);
+	L.marker([lat, lng], { icon: dot }).addTo(map);
+}
+
+let addPolygonBtn = document.getElementById('addPolygon');
+addPolygonBtn.addEventListener('click', () => {
+	console.log('addPolygonBtn');
+	// localStorage.clear();
+	// sessionStorage.clear();
+	map.on('click', addPolygon);
+});
+
+let createPolygonBtn = document.getElementById('createPolygon');
+createPolygonBtn.addEventListener('click', () => {
+	console.log('createPolygonBtn');
+	// sessionStorage.setItem('polygon', JSON.stringify(latlngs));
+	map.off('click', addPolygon);
+
+	latlngs.length
+		? L.polygon(latlngs, { color: 'blue' }).addTo(map)
+		: alert('Нету новых точек');
+
+	latlngs = polygon;
+	// latlngs = [];
+});
